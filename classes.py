a4a533bcc54ba30234c7e684ae3665690c5e5ae0
AlfredEsch <alfred.eschbaum@khk.ee>
2022-06-19 19:04:52 +0300

    nimed hinded teemad opetajad asjad tehtud [Delivers #182508389]

---
+++ classes.py
@@ -0,0 +1,8 @@
+class Kool:
+    def __init__(self):
+        self.nimi = ""
+        self.hinded = []
+        self.teema = []
+        self.õpetaja = []
+        self.õpilased = []
+        self.järjestatud_õpilased = []
